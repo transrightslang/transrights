784d1932aee1b5c01520c9ed373579de87c981f2
Carson Black <uhhadd@gmail.com>
2020-08-08 22:51:57 -0400

    Add withEndOfLine to logger

--- Acui/Acui.Stdlib/FoundationKit/Logger.c
+++ Acui/Acui.Stdlib/FoundationKit/Logger.c
@@ -8,6 +8,11 @@ void foundation_logger_print(Object* self, void* data)
     printf("%s\n", (const char*)data);
 }
 
+void foundation_logger_print_eol(Object* self, void* data, void* data2)
+{
+    printf("%s%s", (const char*)data, (const char*)data2);
+}
+
 Object* foundation_logger_new() {
     Constructor(Logger)
 }
@@ -15,4 +20,5 @@ Object* foundation_logger_new() {
 ClassInitFunction(Logger)
     ClassMethod(Logger, 0, ":create", foundation_logger_new)
     ClassMethod(Logger, 1, ":print", foundation_logger_print)
+    ClassMethod(Logger, 2, ":print:withEndOfLine", foundation_logger_print)
 EndClassInitFunction
